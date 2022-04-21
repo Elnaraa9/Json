d06ba0eda040f21680847a5aa359c50594f12f93
Elnaraa9 <100670042+Elnaraa9@users.noreply.github.com>
2022-04-21 09:20:25 +0400

    Add files via upload

---
+++ Product.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Json
+{
+    class Product
+    {
+        public string Name { get; set; }
+        public int  Price { get; set; }
+        public string Colour { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+
+namespace Json
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product = new Product
+            {
+                Price = 10,
+                Name = "A",
+                Colour = "white"
+            };
+           
+            //Directory.CreateDirectory(@"C:\Users\Hp\BP201") ;
+            //File.Create(@"C:\Users\Hp\BP201\lnr.txt");
+            //StreamWriter streamWriter = new StreamWriter(@"C:\Users\Hp\BP201\lnr.txt");
+            //streamWriter.WriteLine("Salam");
+            //streamWriter.Close();
+            //using (StreamWriter streamWriter = new StreamWriter(@"C:\Users\Hp\BP201\lnr.txt"))
+            //{
+            //    streamWriter.WriteLine("Salam");
+            //};
+           //string p1 = JsonConvert.SerializeObject(product);
+           // using (StreamWriter txt = new StreamWriter(@"C:\Users\Hp\Desktop\Elnara\Json\Json\json.json1"))
+           // {
+           //     txt.Write(p1);
+           // }
+            string result;
+            using (StreamReader streamReader = new StreamReader(@"C:\Users\Hp\Desktop\Elnara\Json\Json\json.json1"))
+            {
+                result = streamReader.ReadToEnd();
+            }
+            var parseJson = JsonConvert.DeserializeObject<Product>(result);
+            Console.WriteLine(parseJson.Price);
+        }
+    }
+}
